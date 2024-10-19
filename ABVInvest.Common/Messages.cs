184663c8100130b94c5de4149722091323b62fa9
Maya Peneva <m.peneva@onebitsoftware.net>
2024-10-19 22:27:53 +0300

    Changed Register page.

--- ABVInvest.Common/Messages.cs
+++ ABVInvest.Common/Messages.cs
@@ -4,6 +4,8 @@
     {
         public static class Identity
         {
+            public const string EmailError = "Моля въведете валиден имейл адрес.";
+            public const string InvalidLogInAttempt = "Неуспешен опит за влизане в сайта.";
             public const string PINError = "ПИН кодът трябва да е дълъг 5 символа и да съдържа само цифри.";
             public const string UsernameError = "Потребителското име трябва да е дълго 5 или 10 символа и да съдържа цифри и/или главни латински букви.";
         }
